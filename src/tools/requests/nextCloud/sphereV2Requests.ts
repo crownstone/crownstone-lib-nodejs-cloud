b152d8a7fb09e670d0c6864567451e990192f7cd
AlexDM0 <alexdemulder@gmail.com>
2022-09-28 13:38:19 +0200

    Fixed endpoints for energyUsage

--- src/tools/requests/nextCloud/sphereV2Requests.ts
+++ src/tools/requests/nextCloud/sphereV2Requests.ts
@@ -5,22 +5,22 @@ import {req} from "../../../util/request";
 export class SphereV2Requests extends RequestorBase {
 
   async getSphereEnergyUsage(sphereId: string, from: timeISOString, until: timeISOString, range: EnergyUsageRange) : Promise<EnergyReturnData[]> {
-    const {body} = await req("GET",`${this.endpoint}sphere/${sphereId}/energyUsage?start=${from}&end=${until}&range=${range}`, this.addSecurity({responseType: 'json' }));
+    const {body} = await req("GET",`${this.endpoint}spheres/${sphereId}/energyUsage?start=${from}&end=${until}&range=${range}`, this.addSecurity({responseType: 'json' }));
     return body as any;
   }
 
   async postSphereEnergyUsage(sphereId: string, energyUsage: EnergyMeasurementData[]) : Promise<StoreReply> {
-    const {body} = await req("POST",`${this.endpoint}sphere/${sphereId}/energyUsage`, this.addSecurity({json: energyUsage, responseType: 'json' }));
+    const {body} = await req("POST",`${this.endpoint}spheres/${sphereId}/energyUsage`, this.addSecurity({json: energyUsage, responseType: 'json' }));
     return body as any;
   }
 
   async getEnergyUploadPermission(sphereId: string) : Promise<boolean> {
-    const {body} = await req("GET",`${this.endpoint}sphere/${sphereId}/energyUsageCollectionPermission`, this.addSecurity({responseType: 'text' }));
+    const {body} = await req("GET",`${this.endpoint}spheres/${sphereId}/energyUsageCollectionPermission`, this.addSecurity({responseType: 'text' }));
     return body === 'true' ? true : false;
   }
 
   async setEnergyUploadPermission(sphereId: string, permission: boolean) : Promise<boolean> {
-    const {body} = await req("POST",`${this.endpoint}sphere/${sphereId}/energyUsageCollectionPermission?permission=${permission}`, this.addSecurity({responseType: 'text' }));
+    const {body} = await req("POST",`${this.endpoint}spheres/${sphereId}/energyUsageCollectionPermission?permission=${permission}`, this.addSecurity({responseType: 'text' }));
     return body === 'true' ? true : false;
   }
 
